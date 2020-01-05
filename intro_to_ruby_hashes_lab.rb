8675dde40b6e8aabb215e63b56182758b8b4e2a1
Firuz Afzalov <afzalov98@gmail.com>
2020-01-05 19:02:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,9 +22,9 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  {:key => value}
+  {key => value}
 end
-my_hash_creator(name, "Messi")
+my_hash_creator(:palyer, "Messi")
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
